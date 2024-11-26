8d4980b1663431d7a3b4276983c95a07bc43de9e
mikeldalmauc <mikeldalmauc@gmail.com>
2024-11-26 20:43:02 +0100

    elementos

--- app/src/main/java/com/example/animations/Elements.kt
+++ app/src/main/java/com/example/animations/Elements.kt
@@ -1,10 +1,14 @@
 package com.example.animations
 
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 
 val Elementos = listOf(
@@ -25,21 +29,33 @@ fun ElementsPreview(){
 fun ElementoAnimado() {
 
     var modifier = Modifier
-        .fillMaxWidth(0.3f)
+        .width(200.dp)
+        .height(200.dp)
 
     LazyColumn {
         Elementos.forEach {
             when (it){
                 R.raw.fire -> item {
                     modifier = modifier
-                        .fillMaxWidth(0.25f)
+                    .padding(16.dp)
+
+                    LottieInfinit(it, modifier)
+                }
+                R.raw.planta -> item {
+                    modifier = modifier
+                        .width(280.dp)
+                        .height(280.dp)
+
+                    LottieInfinit(it, modifier)
+                }
+                R.raw.aire -> item {
+                    modifier = modifier
+                        .padding(32.dp)
 
                     LottieInfinit(it, modifier)
                 }
                 else -> item {
 
-                    modifier = modifier
-                        .fillMaxWidth(0.3f)
                     LottieInfinit(it, modifier)
                 }
             }
